3595bcb87c0fdd9e3207fbd9565343972f27624f
OMAR OUTALEB <ooutaleb5@gmail.com>
2025-11-02 19:14:59 +0100

    Add InfosTouristiques record for structured JSON output

---
+++ src/main/java/com/project/tp_omaroutaleb/llm/InfosTouristiques.java
@@ -0,0 +1,11 @@
+package com.project.tp_omaroutaleb.llm;
+
+import java.util.List;
+
+public record InfosTouristiques(
+        String ville_ou_pays,
+        List<String> endroits_a_visiter,
+        String prix_moyen_repas
+) {}
+
+//Monsieur j'ai a ajouter cette record pour forcer la reponse du format json, j'ai a essaié de le montionner dans le prompte mais même cela il me donne format txt
